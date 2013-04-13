13e04963650631a2f848a4a46b2bd08d3be2547c
Matt Elliott <melliott@ccistudios.com>
2013-04-12 23:26:27 -0400

    menu js added

---
+++ template/templates/warwickTownship/js/menu.js
@@ -0,0 +1,21 @@
+window.addEvent('domready', function() {
+	var displayers = $$('.show-menu'),
+		menu = $$('.mobile-menu')[0],
+		menuContainer = $('.menu'),
+		resize = function() {
+			menu.setStyle('max-height', menu.getElement('ul').getSize().y);
+		};
+
+
+	resize();
+	window.addEvent('resize', function(){
+		resize();
+	});
+
+	displayers.addEvent('click', function(event) {
+		event = new Event(event);
+		event.stop();
+		menu.toggleClass('menu-close');
+		menuContainer.toggleClass('menu-close');
+	});
+});
